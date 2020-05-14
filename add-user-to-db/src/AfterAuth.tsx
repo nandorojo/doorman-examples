df9df5bc88d9066cf6d7edefc28187d2af729374
Fernando Rojo <fernandosrojo@yahoo.com>
2020-05-14 16:03:47 -0400

    add custom doorman ui

--- add-user-to-db/src/AfterAuth.tsx
+++ add-user-to-db/src/AfterAuth.tsx
@@ -6,24 +6,48 @@ import {
   H1,
   Paragraph,
   Input,
+  Header,
 } from 'react-native-doorman'
-import { Button } from 'react-native'
 
 const AfterAuth = () => {
   const { uid, signOut } = useDoormanUser()
+
+  const renderHeader = () => (
+    <Header
+      containerStyle={{
+        backgroundColor: 'transparent',
+        justifyContent: 'space-between',
+        borderBottomWidth: 0,
+      }}
+      centerComponent={{
+        text: 'Welcome!',
+        style: {
+          color: 'white',
+          fontWeight: '500',
+          fontSize: 18,
+        },
+      }}
+      leftComponent={{
+        icon: 'arrow-back',
+        onPress: signOut,
+        color: 'white',
+      }}
+    />
+  )
+
   return (
     <Page
       style={{ marginTop: 100, width: '100%' }}
       background={() => <ScreenBackground color={['green', 'orange']} />}
+      header={renderHeader}
     >
       <H1 centered style={{ color: 'white' }}>
         Welcome to Doorman.
       </H1>
       <Paragraph centered style={{ color: 'white' }}>
-        Sign out below, if {`you'd`} like. Your user id is {uid}.
+        To sign out, click the back arrow above. Your user id is {uid}.
       </Paragraph>
-      <Input />
-      <Button title="Sign Out" color="white" onPress={signOut} />
+      <Input placeholder="Type here!" textAlign="center" />
     </Page>
   )
 }
